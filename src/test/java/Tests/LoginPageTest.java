ae9e286c7962cd47e4cc06adee8892e471f754f2
Nayana Bodhi <nnayana@gmail.com>
2020-05-22 20:44:48 +1000

    Committing changes after adding the comments and moving the Extent Report methods to BaseClass

--- src/test/java/Tests/LoginPageTest.java
+++ src/test/java/Tests/LoginPageTest.java
@@ -38,31 +38,7 @@ import java.util.Map;
 public class LoginPageTest extends BaseClass{
     public static String userName;
     public static String passWord;
-    public ExtentHtmlReporter htmlReporter;
-    public ExtentReports extent;
-    public ExtentTest test;
 
-    @BeforeTest
-    public void extentReportSetup() {
-        //location of the extent report
-        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/ExtentReport.html");
-        extent = new ExtentReports();  //create object of ExtentReports
-        extent.attachReporter(htmlReporter);
-
-        htmlReporter.config().setDocumentTitle("Automation Report"); // Tittle of Report
-        htmlReporter.config().setReportName("Extent Report V4"); // Name of the report
-        htmlReporter.config().setTheme(Theme.DARK);//Default Theme of Report
-
-        // General information releated to application
-        extent.setSystemInfo("Application Name", "http://demoaut.katalon.com/");
-        extent.setSystemInfo("User Name", "Nayana Bodhi");
-        extent.setSystemInfo("Envirnoment", "Test");
-    }
-
-    @AfterTest
-    public void endReport() {
-        extent.flush();
-    }
     @Test
     public void loginTestWithCorrectCredentials() throws InterruptedException {
         test = extent.createTest("login Test With Correct Credentials");
@@ -70,11 +46,11 @@ public class LoginPageTest extends BaseClass{
         LoginPage lp = new LoginPage();
         AppointmentPage ap = new AppointmentPage();
 
+        //Open the Browser and Navigate to the URL
         openBrowser();
 
         //Verify page title
         Assert.assertEquals(DriverFactory.driver.getTitle(),"CURA Healthcare Service");
-
 
         hp.bookAppointmentAction();
         //Verify Login header
@@ -83,9 +59,7 @@ public class LoginPageTest extends BaseClass{
         //Get user credentials
         try {
             getUserCredentials();
-        } catch (YamlException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
+        } catch (YamlException | FileNotFoundException e) {
             e.printStackTrace();
         }
 
@@ -117,6 +91,7 @@ public class LoginPageTest extends BaseClass{
         LoginPage lp = new LoginPage();
         AppointmentPage ap = new AppointmentPage();
 
+        //Open the Browser and Navigate to the URL
         openBrowser();
 
         //Verify page title
@@ -126,59 +101,15 @@ public class LoginPageTest extends BaseClass{
         //Verify Login header
         Assert.assertEquals(ap.getHeader(),"Login");
 
-
         //Login with the user incorrect credentials
         lp.Login("Wrong_Username","Wrong_Password");
 
         Thread.sleep(5000);
         // Verify Page Navigation
-        Assert.assertTrue(DriverFactory.driver.findElement(By.xpath("//p[@class='lead text-danger']")).getText().contains("Login failed!...blahhh"));
+        Assert.assertTrue(DriverFactory.driver.findElement(By.xpath("//p[@class='lead text-danger']")).getText().contains("Login failed!"));
 
         //Close the browser
         closeBrowser();
-    }
-
-    @AfterMethod
-    public void getResult(ITestResult result) throws Exception
-    {
-        if(result.getStatus() == ITestResult.FAILURE)
-        {
-            //MarkupHelper is used to display the output in different colors
-            test.log(Status.FAIL, MarkupHelper.createLabel(result.getName() + " - Test Case Failed", ExtentColor.RED));
-            test.log(Status.FAIL, MarkupHelper.createLabel(result.getThrowable() + " - Test Case Failed", ExtentColor.RED));
-
-            //To capture screenshot path and store the path of the screenshot in the string "screenshotPath"
-            //We do pass the path captured by this method in to the extent reports using "logger.addScreenCapture" method.
-
-            //	String Scrnshot=TakeScreenshot.captuerScreenshot(driver,"TestCaseFailed");
-            String screenshotPath = TakeScreenshot(DriverFactory.driver, result.getName());
-            //To add it in the extent report
-
-            test.fail("Test Case Failed Snapshot is below " + test.addScreenCaptureFromPath(screenshotPath));
-
-
-        }
-        else if(result.getStatus() == ITestResult.SKIP){
-            //logger.log(Status.SKIP, "Test Case Skipped is "+result.getName());
-            test.log(Status.SKIP, MarkupHelper.createLabel(result.getName() + " - Test Case Skipped", ExtentColor.ORANGE));
-        }
-        else if(result.getStatus() == ITestResult.SUCCESS)
-        {
-            test.log(Status.PASS, MarkupHelper.createLabel(result.getName()+" Test Case PASSED", ExtentColor.GREEN));
-        }
-        DriverFactory.driver.quit();
-    }
-
-    public static String TakeScreenshot(WebDriver driver, String screenshotName) throws IOException {
-        String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-        TakesScreenshot ts = (TakesScreenshot) driver;
-        File source = ts.getScreenshotAs(OutputType.FILE);
-
-        // after execution, you could see a folder "FailedTestsScreenshots" under src folder
-        String destination = System.getProperty("user.dir") + "/Screenshots/" + screenshotName + dateName + ".png";
-        File finalDestination = new File(destination);
-        FileUtils.copyFile(source, finalDestination);
-        return destination;
     }
 
     public void getUserCredentials() throws YamlException, FileNotFoundException {
